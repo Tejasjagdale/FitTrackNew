ad1f6137fe1853458592426781fca040327a4abc
tejas <tejasjagdale2003@gmail.com>
2025-11-30 15:16:01 +0530

    andriod v2.0.0

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import ThemeModeProvider from './ThemeModeProvider'
 import './styles.css'
 import App from './App'
+import { StatusBar, Style } from '@capacitor/status-bar';
+import { Capacitor } from '@capacitor/core';
+
+if (Capacitor.getPlatform() === 'android') {
+  StatusBar.setOverlaysWebView({ overlay: false });
+  StatusBar.setStyle({ style: Style.Dark });
+}
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
